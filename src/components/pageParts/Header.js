38fb1e8b0360c74a863439dedbd0f34fa2b841a2
Mo Elwekil <Mo@descod.net>
2021-07-13 23:22:02 +0100

    browser router added to the project

--- src/components/pageParts/Header.js
+++ src/components/pageParts/Header.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import {BrowserRouter, Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className="ui small menu">
-            <a href="/" className="active item">
-                Home
-            </a>
-            <div className="right menu" style={{padding:'10px 0'}}>
-                <button className="ui google red  button" >
-                    <i className="google  icon"></i>
-                    Sign In With Google
-                </button>
+        <BrowserRouter>
+            <div className="ui small menu">
+                <Link to="/" className="active item">
+                    Home
+                </Link>
+                <div className="right menu" style={{padding:'10px 0'}}>
+                    <button className="ui google red  button" >
+                        <i className="google  icon"></i>
+                        Sign In With Google
+                    </button>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     )
 }
 
