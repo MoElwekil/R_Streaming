ea5650663e6ac08ec69eb42143b1f3141a354176
Mo Elwekil <Mo@descod.net>
2021-07-13 23:17:21 +0100

    header warnings fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Header from './page-parts/Header'
+import Header from './pageParts/Header'
 
 const App = () => {
     return (
--- src/components/pageParts/Header.js
+++ src/components/pageParts/Header.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Header = () => {
     return (
         <div className="ui small menu">
-            <a className="active item">
+            <a href="/" className="active item">
                 Home
             </a>
             <div className="right menu" style={{padding:'10px 0'}}>
-                <button class="ui google red  button" >
-                    <i class="google  icon"></i>
+                <button className="ui google red  button" >
+                    <i className="google  icon"></i>
                     Sign In With Google
                 </button>
             </div>
