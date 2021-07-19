1d42068e218cead7cc4eae6d4a8d708869c3b582
Mo Elwekil <Mo@descod.net>
2021-07-20 00:03:19 +0100

    action creators created for creating-updating-deleting stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import stream from '../apis/stream'
-import {SIGNIN, SIGNOUT} from './types'
+import {SIGNIN, SIGNOUT, STREAMS_LIST,STREAM_CREATE,STREAM_DELETE,STREAM_EDIT,STREAM_SHOW} from './types'
 
 export const signIn = () => {
     return {
@@ -16,5 +16,27 @@ export const signOut = () => {
 export const createStream = (formValues) => async (dispatch) => {
     const response = stream.post('/stream', formValues);
 
-    dispatch({type: 'CREATE_STREAM', payload: response.data})
+    dispatch({type: STREAM_CREATE, payload: response.data})
+}
+
+export const streamEdit = (id, formValues) => async(dispatch) => {
+    const response = await stream.put(`/stream/${id}`, formValues)
+
+    dispatch({ type: STREAM_EDIT, payload: response.data})
+}
+
+export const streamDelete = (id) => async(dispatch) => {
+    await stream.delete(`/stream/${id}`)
+
+    dispatch({ type: STREAM_DELETE})
+}
+
+export const streamsList = () => async(dispatch) => {
+    const response = await stream.get('/stream');
+
+    dispatch({ type: STREAMS_LIST, payload: response.data})
+}
+
+export const streamShow = (id) => async(dispatch) => {
+    const response = await stream.get(`/stream/${id}`)
 }
