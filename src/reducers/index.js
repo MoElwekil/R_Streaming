e4d22f48c753a263c20328377d4ee61e5c23db68
Mo Elwekil <Mo@descod.net>
2021-07-23 00:29:05 +0100

    stream reducers added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
-import {reducer as reduxForm} from 'redux-form'
+import { reducer as reduxForm } from "redux-form";
 
 import googleAuthReducer from "./googleAuthReducer";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
-    googleAuth: googleAuthReducer,
-    form: reduxForm
-})
+  googleAuth: googleAuthReducer,
+  form: reduxForm,
+  stream: streamReducer,
+});
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,9 +1,27 @@
+import _ from "lodash";
+import {
+  STREAM_CREATE,
+  STREAM_EDIT,
+  STREAMS_LIST,
+  STREAM_DELETE,
+  STREAM_SHOW,
+} from "../actions/types";
 const streamReducer = (state = {}, action) => {
-    switch(action.type){
-        case 'CREATE_STREAM':
-            return {...state}
+  switch (action.type) {
+    case STREAM_SHOW:
+      return { ...state, [action.payload.id]: action.payload };
+    case STREAM_CREATE:
+      return { ...state, [action.payload.id]: action.payload };
+    case STREAM_EDIT:
+      return { ...state, [action.payload.id]: action.payload };
+    case STREAMS_LIST:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case STREAM_DELETE:
+      return _.omit(state, action.payload);
 
-        default: 
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
+
+export default streamReducer;
